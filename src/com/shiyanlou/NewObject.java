c07d3f603cffabe2516c5609663f698e699368cf
wzlbxznll <41034754+wzlbxznll@users.noreply.github.com>
2018-07-10 15:40:29 +0800

    提交
    测试

--- src/com/shiyanlou/NewObject.java
+++ src/com/shiyanlou/NewObject.java
@@ -4,5 +4,10 @@ public class NewObject {
 
 	public static void main(String[] args) {
 			People WangZhongling =new People(163,22,0);
+			
+			WangZhongling.age=23;
+			WangZhongling.height=160;
+			WangZhongling.printBaseMes();
 	}
+
 }
