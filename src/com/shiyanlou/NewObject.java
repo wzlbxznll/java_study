61c5586b331c15727287d0b549821f8986d5125e
wzlbxznll <41034754+wzlbxznll@users.noreply.github.com>
2018-07-11 16:08:43 +0800

    修改

--- src/com/shiyanlou/NewObject.java
+++ src/com/shiyanlou/NewObject.java
@@ -3,12 +3,12 @@ package com.shiyanlou;
 public class NewObject {
 
 	public static void main(String[] args) {
-			People WangZhongling =new People(163,22,0);
-			WangZhongling.setAge(23);
-			WangZhongling.setHeight(160.0);
-			WangZhongling.printBaseMes();
-			WangZhongling.laugh();
-			System.out.println("WangZhongling的身高是"+WangZhongling.getHeight());
+//			People WangZhongling =new People(163,22,0);
+//			WangZhongling.setAge(23);
+//			WangZhongling.setHeight(160.0);
+//			WangZhongling.printBaseMes();
+//			WangZhongling.laugh();
+//			System.out.println("WangZhongling的身高是"+WangZhongling.getHeight());
 	}
 
 }
