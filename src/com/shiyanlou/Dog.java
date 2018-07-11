0dcbf33eeb6f0cd3f105816fa783fabcbc7e036b
wzlbxznll <41034754+wzlbxznll@users.noreply.github.com>
2018-07-11 16:31:04 +0800

    重写父类方法
    test类为测试

--- src/com/shiyanlou/Dog.java
+++ src/com/shiyanlou/Dog.java
@@ -1,5 +1,8 @@
 package com.shiyanlou;
 
 public class Dog extends Animal {
-
+			
+			public void bark() {
+				System.out.println("汪！汪！汪！");
+			}
 }
--- src/com/shiyanlou/Test.java
+++ src/com/shiyanlou/Test.java
@@ -3,9 +3,15 @@ package com.shiyanlou;
 public class Test {
 
 	public static void main(String[] args) {
-			Dog a =new Dog();
-			a.legNum=4;
+//			Dog a =new Dog();
+//			a.legNum=4;
+//			a.bark();
+			Animal a=new Animal();
+			Dog d=new Dog();
+			Animal b= new Dog();
 			a.bark();
+			d.bark();
+			b.bark();
 	}
 
 }
